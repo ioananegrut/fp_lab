ffae187ea2538bfd661f414c557276d566b3b11a
ioana_negrut <ioana.negrut@gmail.com>
2023-10-05 08:13:23 +0200

    working app on local with db login

--- lab_app.py
+++ lab_app.py
@@ -2,37 +2,38 @@ import streamlit as st
 import pandas as pd
 import streamlit_authenticator as stauth
 from supabase import create_client
-import yaml
-from yaml import SafeLoader
 
+# Initialize database connection
+@st.cache_resource
+def init_connection():
+    url = st.secrets["supabase_url"]
+    key = st.secrets["supabase_key"]
+    return create_client(url, key)
 
+supabase = init_connection()
 ## Add student log-in
 
 ## Get the encrypted user credentials from the DB
-# def run_user_query():
-#     print("getting credentials table from db")
-#     return supabase.table("credentials").select("*").execute()
-# credentials_rows = run_user_query()
-# credentials_df = pd.DataFrame(credentials_rows.data)
+def run_user_query():
+    print("getting credentials table from db")
+    return supabase.table("credentials").select("*").execute()
+credentials_rows = run_user_query()
+credentials_df = pd.DataFrame(credentials_rows.data)
 
-## Format the credentials as required
-# config={"credentials":{"usernames":{}}}
-# for index, row in credentials_df.iterrows():
-#     user_dict = {"name":row["name"],
-#                  "password": [row["password"]]}
-#     config["credentials"]["usernames"][row["username"]]=user_dict
-# print(config)
+# Format the credentials as required by Streamlit authenticator
+config={"credentials":{"usernames":{}}}
+for index, row in credentials_df.iterrows():
+    user_dict = {"name":row["name"],
+                 "password": row["password"]}
+    config["credentials"]["usernames"][row["username"]]=user_dict
 
-config = {"credentials": {"usernames": {"a1": {"name": "anna",
-                                               "password": "abc"}}}}
-config_s = yaml.dump(config)
-config_ss = yaml.load(config_s, Loader=SafeLoader)
 # AUTHENTICATE in the app
-authenticator = stauth.Authenticate(config_ss["credentials"],
+authenticator = stauth.Authenticate(config["credentials"],
                                     "blabla", 
                                     "xx", 
                                     cookie_expiry_days=0)
 name, authentication_status, username = authenticator.login("Login", "main")
+print(name, username, authentication_status)
 
 if authentication_status == False:
     st.error("Username/password incorecte")
@@ -43,23 +44,16 @@ if authentication_status:
     # Add a logout button
     authenticator.logout("Logout", "sidebar")
 
-    # Initialize database connection
-    @st.cache_resource
-    def init_connection():
-        url = st.secrets["supabase_url"]
-        key = st.secrets["supabase_key"]
-        return create_client(url, key)
-
-    supabase = init_connection()
-
-    st.title("Laborator Fundamentele Programării - student {}".format(name))
+    
+    st.title("Welcome {}!".format(name))
+    st.header("Laborator Fundamentele Programării")
 
     # Fetch the student data from the database
     def run_query():
         return supabase.table("fplab").select("*").execute()
     rows = run_query()
     data = pd.DataFrame(rows.data)
-    student_data = data[data["student_id"]==name]
+    student_data = data[data["student_id"]==username]
 
     # Display the data 
     st.subheader("Evoluția notelor:")
